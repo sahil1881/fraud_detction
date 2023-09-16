fb721d408d633671acdf4f1bf59419ab9e388f89
sahil1881 <sahil1047@gmail.com>
2023-09-17 00:33:33 +0530

    added new Model1,model2,model3

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,15 @@
 
 
 print("adding all ML realted code here")
+
+def model1():
+    print("adding code fror ML model1")
+
+def model2():
+    print("adding code fror ML model2")
+
+
+
+def model3():
+    print("adding code fror ML model3")
+
